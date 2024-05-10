bfc2fadb21dfff30b376bac4f5c4d50fbcdae688
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-10 06:59:23 +0200

    created a data.json file and fetched the data via an api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import Link from "next/link";
-import data from "../../data/data";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/stories')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
+
+export default async function Home() {
+  const stories = await getData()
+  const { data } = stories
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section>Figure out how successful designers got their start.</section>
-      {data.map((designer: any, index) => {
+      {data.map((designer: any, index: any) => {
         return (
-          <Link href={`${designer.lastName.toLowerCase()}-${designer.firstName.toLowerCase()}`}>
+          <Link href={`/stories/${designer.lastName.toLowerCase()}-${designer.firstName.toLowerCase()}`}>
             <div>
               <p>{index + 1}</p>
               <h1>{designer.firstName} {designer.lastName}</h1>

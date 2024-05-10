e82e3542b5cf4d36cdb80872563796ad7cf94a08
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-10 19:45:55 +0200

    added prettierrc file and working on tailwindcss

--- src/app/(pages)/tools/page.tsx
+++ src/app/(pages)/tools/page.tsx
@@ -1,7 +1,7 @@
 export default function Tools() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>hello world</h1>
-        </main>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="bg-slate-400 p-11">Tools</h1>
+        </div>
     )
 }
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export function Navbar() {
+    return (
+        <div className="flex justify-between px-8 py-4 w-screen bg-slate-400">
+            <Link href="/">Designer Stories</Link>
+            <div className="flex gap-4">
+                <Link href="/about">About</Link>
+                <Link href="/stories">Stories</Link>
+                <Link href="/tools">Tools</Link>
+                <Link href="blog">Blog</Link>
+            </div>
+        </div>
+    )
+}

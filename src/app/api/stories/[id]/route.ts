1ef2b95763437bb0f1fd246436695c158a8ac93a
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-10 18:50:01 +0200

    figured out how to do dynamic routing

---
+++ src/app/api/stories/[id]/route.ts
@@ -0,0 +1,9 @@
+import data from '../../../../../data/data.json'
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  const story = data.find((comment) => comment.id === parseInt(params.id))
+  return Response.json(story)
+}
--- src/app/stories/[id]/page.tsx
+++ src/app/stories/[id]/page.tsx
@@ -1,21 +1,24 @@
-async function getData(postId: string) {
+async function getData(id: string) {
     // TODO: Figure out why this is not working
-    const res = await fetch(`http://localhost:3000/api/stories/${postId}`)
+    const res = await fetch(`http://localhost:3000/api/stories/${id}`)
     const data = await res.json()
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
-    return data.data
+    return data
 }
 
 export default async function Story({ params }: any) {
-    const info = await getData(params.id)
+    const story = await getData(params.id)
 
-    console.log(info);
+    const { id, firstName, lastName, datePosted, contact, info } = story
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <p>{params.id}</p>
+            <p>{id}</p>
+            <p>{firstName}</p>
+            <p>{lastName}</p>
+            <p>{info.advice}</p>
         </main>
     );
 }

d97c883597c3c061d7e49b9cc9d2db31ec2dc3f7
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-11 10:12:20 +0200

    added cache: no store to fetch

--- src/app/(pages)/stories/page.tsx
+++ src/app/(pages)/stories/page.tsx
@@ -1,7 +1,23 @@
-export default function About() {
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/stories')
+  const data = await res.json()
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return data.data
+}
+export default async function About() {
+  const data = await getData()
+
+
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Stories</h1>
+      {data.map((designer: any) => {
+        return <div>{designer.firstName}</div>
+      })}
+
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
 async function getData() {
-  const res = await fetch('http://localhost:3000/api/stories')
+  const res = await fetch('http://localhost:3000/api/stories', { cache: 'no-store' })
   const data = await res.json()
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
-  return data.data
+  return data
 }
 
 export default async function Home() {
   const data = await getData()
+  console.log(data);
 
   return (
     <>
-      <section className="w-100 h-80 grid place-items-center">
+      <section className="w-100 h-100 grid place-items-center">
         <h1 className="text-4xl font-bold" >
           Figure out how successful designers got their start.
         </h1>
@@ -22,22 +24,26 @@ export default async function Home() {
       {
         data.map((designer: any, index: any) => {
           return (
-            <Link href={`/stories/${designer.id}`}>
-              <div>
-                <p>{index + 1}</p>
-                <h1>{designer.firstName} {designer.lastName}</h1>
-                {designer.info.company ?
-                  <p>{designer.info.position} at {designer.info.company}</p> :
-                  <p>Self-employed</p>
-                }
-                <p>{designer.datePosted}</p>
-                {designer.info.yearlySalary ?
-                  <p>{designer.info.yearlySalary}</p> :
-                  <p>Non-disclosed</p>
-                }
+            <Suspense fallback={"Loading..."}>
+              <div key={designer.id}>
+                <Link href={`/stories/${designer.id}`}>
+                  <div>
+                    <p>{index + 1}</p>
+                    <h1>{designer.firstName} {designer.lastName}</h1>
+                    {designer.info.company ?
+                      <p>{designer.info.position} at {designer.info.company}</p> :
+                      <p>Self-employed</p>
+                    }
+                    <p>{designer.datePosted}</p>
+                    {designer.info.yearlySalary ?
+                      <p>{designer.info.yearlySalary}</p> :
+                      <p>Non-disclosed</p>
+                    }
 
+                  </div>
+                </Link>
               </div>
-            </Link>
+            </Suspense>
           )
         })
       }

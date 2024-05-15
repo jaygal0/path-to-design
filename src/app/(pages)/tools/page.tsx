f7d5c863ab9a215aaa13ffcee2fbff3185095de1
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-15 09:54:25 -0700

    removed tools page

--- src/app/(pages)/tools/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Tools() {
-    return (
-        <div className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="bg-slate-400 p-11">Tools</h1>
-        </div>
-    )
-}

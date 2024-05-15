88aecedf1ece36a0ed496d80f1a922997d7c28a7
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-15 10:50:28 -0700

    z index on nav

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Navbar() {
     return (
-        <div className="fixed top-0 left-0 flex justify-between px-8 py-8 w-screen">
+        <div className="z-50 fixed top-0 left-0 flex justify-between px-8 py-8 w-screen">
             <Link href="/">Designer Stories</Link>
             <div className="flex gap-7">
                 <Link href="/about">About</Link>

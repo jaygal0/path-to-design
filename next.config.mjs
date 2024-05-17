b8b6fdedc64312242f276974bcf1c5de51b3dad2
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-17 12:44:46 -0700

    managed to get the basic /mdx-page to work

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
+import createMDX from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['dummyimage.com'],
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
-export default nextConfig
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+export default withMDX(nextConfig)

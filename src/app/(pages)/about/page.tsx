b533137ac0b423bd9b9d25249fbc4a9f0efcb116
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-27 20:40:05 -0700

    sorted out a basic about page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,7 +1,12 @@
 export default function About() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>About</h1>
+    <div className="flex min-h-screen flex-col p-24">
+      <h1 className="text-2xl font-bold">About</h1>
+      <div className="flex gap-4">
+        <div className="h-72 w-1/2 bg-black"></div>
+        <p className="w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas consequatur harum, distinctio exercitationem mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero quo numquam dolor, amet exercitationem quos qui odit molestiae quia asperiores ex nesciunt quod consectetur quaerat? Incidunt?</p>
+      </div>
+
     </div>
   )
 }

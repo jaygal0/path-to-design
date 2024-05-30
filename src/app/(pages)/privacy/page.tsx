d09bd41afa883763c5e3add5fbea1282dcef4411
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-30 10:38:26 -0700

    updated mobile responsive design for privacy and share pages

--- src/app/(pages)/privacy/page.tsx
+++ src/app/(pages)/privacy/page.tsx
@@ -1,14 +1,57 @@
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col p-24">
-      <h1 className="mb-4 text-2xl font-bold">Privacy</h1>
-      <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas consequatur harum, distinctio exercitationem mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero quo numquam dolor, amet exercitationem quos qui odit molestiae quia asperiores ex nesciunt quod consectetur quaerat? Incidunt?</p>
-      <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas consequatur harum, distinctio exercitationem mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero quo numquam dolor, amet exercitationem quos qui odit molestiae quia asperiores ex nesciunt quod consectetur quaerat? Incidunt?</p>
-      <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas consequatur harum, distinctio exercitationem mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero quo numquam dolor, amet exercitationem quos qui odit molestiae quia asperiores ex nesciunt quod consectetur quaerat? Incidunt?</p>
-      <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas consequatur harum, distinctio exercitationem mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero quo numquam dolor, amet exercitationem quos qui odit molestiae quia asperiores ex nesciunt quod consectetur quaerat? Incidunt?</p>
-      <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas consequatur harum, distinctio exercitationem mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero quo numquam dolor, amet exercitationem quos qui odit molestiae quia asperiores ex nesciunt quod consectetur quaerat? Incidunt?</p>
-      <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas consequatur harum, distinctio exercitationem mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero quo numquam dolor, amet exercitationem quos qui odit molestiae quia asperiores ex nesciunt quod consectetur quaerat? Incidunt?</p>
-      <p className="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas consequatur harum, distinctio exercitationem mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero quo numquam dolor, amet exercitationem quos qui odit molestiae quia asperiores ex nesciunt quod consectetur quaerat? Incidunt?</p>
+    <div className="flex flex-col">
+      <h1 className="my-20 text-3xl font-bold">Privacy</h1>
+      <div className="flex flex-col gap-7">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, quas consequatur harum, distinctio exercitationem
+          mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut
+          doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero
+          quo numquam dolor, amet exercitationem quos qui odit molestiae quia
+          asperiores ex nesciunt quod consectetur quaerat? Incidunt?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, quas consequatur harum, distinctio exercitationem
+          mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut
+          doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero
+          quo numquam dolor, amet exercitationem quos qui odit molestiae quia
+          asperiores ex nesciunt quod consectetur quaerat? Incidunt?
+        </p>{" "}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, quas consequatur harum, distinctio exercitationem
+          mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut
+          doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero
+          quo numquam dolor, amet exercitationem quos qui odit molestiae quia
+          asperiores ex nesciunt quod consectetur quaerat? Incidunt?
+        </p>{" "}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, quas consequatur harum, distinctio exercitationem
+          mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut
+          doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero
+          quo numquam dolor, amet exercitationem quos qui odit molestiae quia
+          asperiores ex nesciunt quod consectetur quaerat? Incidunt?
+        </p>{" "}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, quas consequatur harum, distinctio exercitationem
+          mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut
+          doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero
+          quo numquam dolor, amet exercitationem quos qui odit molestiae quia
+          asperiores ex nesciunt quod consectetur quaerat? Incidunt?
+        </p>{" "}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, quas consequatur harum, distinctio exercitationem
+          mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut
+          doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero
+          quo numquam dolor, amet exercitationem quos qui odit molestiae quia
+          asperiores ex nesciunt quod consectetur quaerat? Incidunt?
+        </p>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/(pages)/share/page.tsx
+++ src/app/(pages)/share/page.tsx
@@ -1,8 +1,11 @@
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col p-24">
-      <h1 className="mb-4 text-2xl font-bold">Share Your Story</h1>
-      <p className="mb-3">If you'd like to share your story or simply get in touch, you can send an email at email@email.com</p>
+    <div className="flex flex-col">
+      <h1 className="my-20 text-3xl font-bold">Share Your Story</h1>
+      <p className="mb-3">
+        If you'd like to share your story or simply get in touch, you can send
+        an email at email@email.com
+      </p>
     </div>
-  )
+  );
 }

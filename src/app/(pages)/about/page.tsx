c5689488d53e5d4fa0cd330c2c552c533c530661
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-05-30 10:34:57 -0700

    responsive mobile about

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,12 +1,18 @@
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col p-24">
-      <h1 className="text-2xl font-bold">About</h1>
-      <div className="flex gap-4">
-        <div className="h-72 w-1/2 bg-black"></div>
-        <p className="w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas consequatur harum, distinctio exercitationem mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero quo numquam dolor, amet exercitationem quos qui odit molestiae quia asperiores ex nesciunt quod consectetur quaerat? Incidunt?</p>
+    <div className="flex flex-col">
+      <h1 className="my-20 text-3xl font-bold">About</h1>
+      <div className="flex flex-col gap-4">
+        <div className="aspect-video h-72 bg-black"></div>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, quas consequatur harum, distinctio exercitationem
+          mollitia eos, at sunt eveniet sit eius praesentium impedit sapiente ut
+          doloremque? Accusamus ipsum, laboriosam, facilis sunt repudiandae vero
+          quo numquam dolor, amet exercitationem quos qui odit molestiae quia
+          asperiores ex nesciunt quod consectetur quaerat? Incidunt?
+        </p>
       </div>
-
     </div>
-  )
+  );
 }

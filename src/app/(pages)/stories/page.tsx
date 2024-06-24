fed255fb2f988db046cf75a0f04d8b3f74bf41f8
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-06-24 23:03:57 +0200

    hide stories page

--- src/app/(pages)/stories/page.tsx
+++ src/app/(pages)/stories/page.tsx
@@ -1,20 +1,20 @@
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/stories");
-  const data = await res.json();
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return data;
-}
-export default async function About() {
-  const data = await getData();
+// async function getData() {
+//   const res = await fetch("http://localhost:3000/api/stories");
+//   const data = await res.json();
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   return data;
+// }
+// export default async function About() {
+//   const data = await getData();
 
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Stories</h1>
-      {data.map((designer: any) => {
-        return <div key={designer.id}>{designer.firstName}</div>;
-      })}
-    </div>
-  );
-}
+//   return (
+//     <div className="flex min-h-screen flex-col items-center justify-between p-24">
+//       <h1>Stories</h1>
+//       {data.map((designer: any) => {
+//         return <div key={designer.id}>{designer.firstName}</div>;
+//       })}
+//     </div>
+//   );
+// }

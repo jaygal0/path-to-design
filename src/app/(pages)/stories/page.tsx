3ceaed5484127629b22c144fb7a7ce1747b43d3d
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-06-24 23:07:12 +0200

    updated stories to cater to Vercel

--- src/app/(pages)/stories/page.tsx
+++ src/app/(pages)/stories/page.tsx
@@ -1,20 +1,21 @@
-// async function getData() {
-//   const res = await fetch("http://localhost:3000/api/stories");
-//   const data = await res.json();
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data");
-//   }
-//   return data;
-// }
-// export default async function About() {
-//   const data = await getData();
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/stories");
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return data;
+}
 
-//   return (
-//     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <h1>Stories</h1>
-//       {data.map((designer: any) => {
-//         return <div key={designer.id}>{designer.firstName}</div>;
-//       })}
-//     </div>
-//   );
-// }
+export default async function Stories() {
+  const data = await getData();
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Stories</h1>
+      {data.map((designer: any) => {
+        return <div key={designer.id}>{designer.firstName}</div>;
+      })}
+    </div>
+  );
+}

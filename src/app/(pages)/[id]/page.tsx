2ff17f21eebc2dd4032bace7f9d8d874610912b7
Joshua <joshuagalinato@gmail.com>
2024-06-25 09:07:09 +0200

    updated leading in /page site

--- src/app/(pages)/[id]/page.tsx
+++ src/app/(pages)/[id]/page.tsx
@@ -20,7 +20,7 @@ export default async function Story({ params }: any) {
 
   return (
     <div className="flex flex-col gap-10">
-      <h1 className="mb-20 mt-40 text-4xl font-bold leading-normal md:text-6xl">
+      <h1 className="mb-20 mt-40 text-4xl font-bold leading-normal md:text-6xl md:leading-tight">
         &quot;{info.oneLiner}&quot;
       </h1>
       <DesignerDetailBox

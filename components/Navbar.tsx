baf653fec291d036013b0e009b350d76754f6ac1
jaygal <jaygal@Joshuas-MacBook-Pro.local>
2024-07-02 18:34:21 +0200

    added newsletter to the mobile sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import Link from "next/link";
+import { MobileSidebar } from "./MobileSidebar";
 
 export function Navbar() {
   const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false);
@@ -17,7 +18,7 @@ export function Navbar() {
       }
     };
 
-    document.addEventListener("click", closeMobileMenuOnClickOutside);
+    //    document.addEventListener("click", closeMobileMenuOnClickOutside);
 
     return () => {
       document.removeEventListener("click", closeMobileMenuOnClickOutside);
@@ -29,33 +30,7 @@ export function Navbar() {
       <Link className="text-xl font-bold" href="/">
         Path To Design
       </Link>
-      {showMobileMenu && (
-        <div className="absolute left-0 top-0 h-screen w-5/6 border-r-2 border-white bg-stone-950">
-          <svg
-            className="absolute right-4 top-4"
-            onClick={toggleMobileMenu}
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-          >
-            <g fill="none" fillRule="evenodd">
-              <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
-              <path
-                fill="currentColor"
-                d="m12 14.122l5.303 5.303a1.5 1.5 0 0 0 2.122-2.122L14.12 12l5.304-5.303a1.5 1.5 0 1 0-2.122-2.121L12 9.879L6.697 4.576a1.5 1.5 0 1 0-2.122 2.12L9.88 12l-5.304 5.304a1.5 1.5 0 1 0 2.122 2.12z"
-              />
-            </g>
-          </svg>
-          <div className="flex flex-col gap-8 p-7 text-4xl">
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/privacy">Privacy</Link>
-            <Link href="/share">Share Your Story</Link>
-          </div>
-        </div>
-      )}
+      {showMobileMenu && <MobileSidebar state={toggleMobileMenu} />}
       {!showMobileMenu && (
         <svg
           onClick={toggleMobileMenu}
@@ -75,10 +50,15 @@ export function Navbar() {
           />
         </svg>
       )}
-      <div className="hidden gap-7 pr-4 font-sans md:flex items-center">
+      <div className="hidden items-center gap-7 pr-4 font-sans md:flex">
         <Link href="/about">About</Link>
         <Link href="/blog">Blog</Link>
-        <Link href="/share" className="btn-gradient text-stone-950 p-1 px-4 rounded-md">Share Your Story</Link>
+        <Link
+          href="/share"
+          className="btn-gradient rounded-md p-1 px-4 text-stone-950"
+        >
+          Share Your Story
+        </Link>
       </div>
     </div>
   );

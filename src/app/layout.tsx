be6c835eb50dd4d120ad3daa616494844513a2a6
Joshua <joshuagalinato@gmail.com>
2024-11-05 11:36:14 +0100

    added rows to cater to the guidelines

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,9 +35,19 @@ export default function RootLayout({
       >
         <Navbar />
         <main className="grid min-h-screen place-items-center">
-          <div className="grid max-w-screen-2xl grid-cols-12 gap-4 px-4">
-            {/* TODO:Figure out the grid line issues */}
-            <div className="-z-10 col-span-1 col-start-1 bg-gray-500 opacity-5" />
+          <div className="grid max-w-screen-2xl grid-cols-12 grid-rows-[min-content_min-content_min-content_min-content_min-content_min-content] gap-4 px-4">
+            <div className="-z-10 col-span-1 col-start-1 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-2 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-3 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-4 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-5 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-6 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-7 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-8 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-9 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-10 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-11 row-span-full row-start-1 bg-gray-500 opacity-5" />
+            <div className="-z-10 col-span-1 col-start-12 row-span-full row-start-1 bg-gray-500 opacity-5" />
             {children}
           </div>
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,47 +21,89 @@ export default async function Home() {
 
   return (
     <>
-      <Hero />
-      <CalloutMain />
-      <NewsletterFormBox />
-      <CalloutAlt />
-      <Section>
-        <div className="col-start-2 col-end-10 flex flex-col gap-8">
-          {designers.map((designer: any, index: any) => {
-            const {
-              companies,
-              countries,
-              createdAt,
-              firstName,
-              id,
-              isPublished,
-              lastName,
-              roles,
-              salaries,
-              slug,
-              updatedAt,
-            } = designer;
-            return (
-              <div key={id}>
-                {isPublished && (
-                  <CardDesigner
-                    company={companies.company}
-                    country={countries.country}
-                    createdAt={createdAt}
-                    firstName={firstName}
-                    id={id}
-                    lastName={lastName}
-                    role={roles.role}
-                    salary={salaries.salary}
-                    slug={slug}
-                    updatedAt={updatedAt}
-                  />
-                )}
-              </div>
-            );
-          })}
-        </div>
-      </Section>
+      <h1 className="text-gradient col-span-7 col-start-2 row-span-1 row-start-1 mb-8 h-[100vh] max-h-[1080px] content-end pb-12 text-xl font-bold leading-tight lg:text-5xl lg:leading-tight">
+        Helping designers navigate the industry by following the paths of
+        successful designers.
+      </h1>
+      <h2 className="col-start-4 col-end-11 row-span-1 row-start-2 my-20 text-4xl font-bold lg:text-7xl lg:leading-tight">
+        We understand that it can be hard to{" "}
+        <span className="text-gradient">find work</span>,{" "}
+        <span className="text-gradient">be creative</span>, or{" "}
+        <span className="text-gradient">stay inspired</span> as a designer.
+      </h2>
+      <p className="col-start-6 col-end-11 row-span-1 row-start-3 my-10 text-2xl">
+        That’s why we built this site – to give you insight into what successful
+        designers are doing to thrive in the industry, so you no longer have to
+        worry about where to find this information.
+      </p>
+      <div className="col-start-4 col-end-10 row-span-1 row-start-4 my-20 h-min rounded-2xl border p-8">
+        <h4 className="mb-2 w-full pb-6 text-3xl font-normal">
+          Stay inspired and discover the paths of more designers.
+        </h4>
+        <form
+          className="flex w-full flex-col"
+          action="https://pathtodesign.us17.list-manage.com/subscribe/post"
+          method="POST"
+        >
+          <input type="hidden" name="u" value="e41d9cf2ed34317e99e5891b9" />
+          <input type="hidden" name="id" value="9437e60fa0" />
+          <div className="flex gap-4">
+            <input
+              className="w-full border-b-2 border-black p-2 text-stone-950"
+              type="email"
+              id="MERGE0"
+              name="MERGE0"
+              placeholder="name@email.com"
+            />
+            <button
+              className="btn-gradient mt-2 w-2/3 cursor-pointer rounded-sm p-2 text-stone-950 hover:bg-slate-300"
+              type="submit"
+            >
+              Subscribe
+            </button>
+          </div>
+        </form>
+      </div>
+      <h2 className="col-span-6 col-start-2 row-span-1 row-start-5 my-20 text-xl font-bold leading-tight lg:text-5xl lg:leading-tight">
+        <span className="text-gradient">Take charge of your career</span> by
+        understanding the paths designers took to be{" "}
+        <span className="text-gradient">successful</span>.
+      </h2>
+      <div className="col-start-4 col-end-9 row-span-1 row-start-6 mb-40 flex flex-col gap-8">
+        {designers.map((designer: any, index: any) => {
+          const {
+            companies,
+            countries,
+            createdAt,
+            firstName,
+            id,
+            isPublished,
+            lastName,
+            roles,
+            salaries,
+            slug,
+            updatedAt,
+          } = designer;
+          return (
+            <div key={id}>
+              {isPublished && (
+                <CardDesigner
+                  company={companies.company}
+                  country={countries.country}
+                  createdAt={createdAt}
+                  firstName={firstName}
+                  id={id}
+                  lastName={lastName}
+                  role={roles.role}
+                  salary={salaries.salary}
+                  slug={slug}
+                  updatedAt={updatedAt}
+                />
+              )}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }

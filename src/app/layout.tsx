3eb04b8c29357c2c414bb34725d1430a3bb44693
jaygal <joshuagalinato@gmail.com>
2025-01-02 12:44:45 +0100

    included og:image, title and desc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,22 @@ export const metadata: Metadata = {
   title: "Path to Design | Unlock Your Creative Potential in Design",
   description:
     "Discover how designers start their journey, excel in their careers, and find inspiration to create impactful designs. Explore resources, stories, and strategies at Path to Design.",
+  openGraph: {
+    title: "Path to Design | Unlock Your Creative Potential in Design",
+    description:
+      "Discover how designers start their journey, excel in their careers, and find inspiration to create impactful designs. Explore resources, stories, and strategies at Path to Design.",
+    url: "https://www.pathtodesign.com",
+    siteName: "Path to Design",
+    images: [
+      {
+        url: "/path-to-design-og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Path to Design",
+      },
+    ],
+    locale: "en",
+  },
 };
 
 export default function RootLayout({

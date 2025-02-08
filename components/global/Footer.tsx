c991f7abe869950fe8defd8c53a43a4f496cee44
jaygal <joshuagalinato@gmail.com>
2025-02-09 00:41:10 +0100

    updated footer

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
   return (
     <footer className="w-full border-t-2 border-gray-200 bg-stone-950 p-4 font-sans shadow md:flex md:items-center md:justify-between md:p-6">
       <div className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        ©{dayjs().year()}{" "}
-        <span className="text-md font-sans">Path to Design</span>. All Rights
-        Reserved.
+        © {dayjs().year()}{" "}
+        <span className="text-md text-gradient font-sans">Path to Design</span>.
+        All Rights Reserved.
       </div>
       <ul className="mt-3 flex flex-wrap items-center gap-10 font-sans text-sm font-medium text-gray-500 sm:mt-0 dark:text-gray-400">
         <li>

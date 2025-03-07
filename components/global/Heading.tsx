b56a6f3773175b18aca80a36fff4908d21a5f2fe
jaygal <joshuagalinato@gmail.com>
2025-03-07 16:19:48 +0100

    sorted out leading in title issue

--- components/global/Heading.tsx
+++ components/global/Heading.tsx
@@ -8,7 +8,7 @@ export function Heading({ heading, desc, isSecondary }: Props) {
   return (
     <>
       <h1
-        className={`text-gradient mb-8 ${isSecondary ? "text-2xl" : "text-4xl"} font-bold leading-tight md:${isSecondary ? "text-5xl" : "text-6xl"} md:leading-normal lg:${isSecondary ? "text-6xl" : "text-7xl"}`}
+        className={`text-gradient mb-8 ${isSecondary ? "text-2xl" : "text-4xl"} font-bold leading-tight md:${isSecondary ? "text-5xl" : "text-6xl"} md:leading-normal lg:${isSecondary ? "text-6xl" : "text-7xl"} lg:leading-[1.4]`}
       >
         {heading}
       </h1>

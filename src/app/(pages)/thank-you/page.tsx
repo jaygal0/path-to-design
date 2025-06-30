761fc6739945d633d69ba19445819f27eff77893
jaygal <joshuagalinato@gmail.com>
2025-06-30 19:42:56 +0200

    updated padding

--- src/app/(pages)/thank-you/page.tsx
+++ src/app/(pages)/thank-you/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="md:px-96">
+    <div className="xl:px-96">
       <Heading
         heading="Thank You!"
         desc="Your submission has been received. We'll get back to you as soon as possible to let you know when your profile is live."

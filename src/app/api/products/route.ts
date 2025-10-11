08d38d048d12f6489c06dfbdb6091c9033f9c594
jaygal <joshuagalinato@gmail.com>
2025-10-11 10:36:47 +0200

    added trello and products api

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,21 @@
+import prisma from "@/lib/db";
+
+export async function GET(req: Request) {
+  try {
+    const data = await prisma.products.findMany({
+      include: {
+        designers: true,
+      },
+    });
+
+    // Sort the data based on the length of designers array
+    const sortedData = data.sort(
+      (a, b) => b.designers.length - a.designers.length,
+    );
+
+    return Response.json(sortedData);
+  } catch (error) {
+    console.error("Failed to fetch apps:", error);
+    return Response.json({ error: "Failed to fetch apps" }, { status: 500 });
+  }
+}

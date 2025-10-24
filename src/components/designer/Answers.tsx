cb991caf1e1de4418e388428dc622760149bce70
jaygal <joshuagalinato@gmail.com>
2025-10-24 12:47:44 +0200

    Included markdown

--- src/components/designer/Answers.tsx
+++ src/components/designer/Answers.tsx
@@ -1,14 +1,17 @@
+import ReactMarkdown from "react-markdown";
+
 export interface QuestionAndAnswers {
-  question: String;
-  answer: [];
+  question: string;
+  answer: string; // should be string, not []
 }
+
 export function Answers({ question, answer }: QuestionAndAnswers) {
   return (
     <div className="w-full">
       <h3 className="mb-2 text-xl text-muted-foreground">{question}</h3>
-      <p className="mb-6 whitespace-pre-wrap font-sans text-xl leading-relaxed">
-        {answer}
-      </p>
+      <div className="prose prose-invert max-w-none text-xl leading-relaxed">
+        <ReactMarkdown>{answer}</ReactMarkdown>
+      </div>
     </div>
   );
 }

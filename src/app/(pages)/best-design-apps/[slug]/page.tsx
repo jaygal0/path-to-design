428ff915a37ebd470ca2a50e4c5358637bf7e573
jaygal <joshuagalinato@gmail.com>
2025-10-30 07:33:49 +0100

    added books sidebar

--- src/app/(pages)/best-design-apps/[slug]/page.tsx
+++ src/app/(pages)/best-design-apps/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import BrowseApp from "@/components/directory/BrowseApp";
+import { PopularBooksSidebar } from "@/components/global/PopularBooksSidebar";
+import { PopularApps } from "@/components/home/PopularApps";
 import { notFound } from "next/navigation";
 
 export default async function AppDetailPage({
@@ -17,5 +19,26 @@ export default async function AppDetailPage({
 
   const app = await res.json();
 
-  return <BrowseApp app={app} />;
+  const booksRes = await fetch(`${process.env.WEB_SITE}/api/books`, {
+    next: { revalidate: 60 },
+  });
+
+  if (!booksRes.ok) {
+    throw new Error("Failed to fetch books data");
+  }
+
+  const booksData = await booksRes.json();
+
+  return (
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+      <div className="col-span-2">
+        <BrowseApp app={app} />
+      </div>
+      <div className="col-span-1">
+        <div className="sticky top-8">
+          <PopularBooksSidebar books={booksData} />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/global/PopularBooksSidebar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ChevronRight } from "lucide-react";
+import BookItem from "@/components/global/BookItem";
+import { mainCTAs } from "@/config/navigation";
+
+export function PopularBooksSidebar({ books }: any) {
+  return (
+    <div className="col-span-1 h-fit rounded-2xl bg-neutral-900 p-3 md:p-6 xl:col-span-3">
+      <div className="mb-6">
+        <div className="mb-1 flex items-center justify-between gap-1">
+          <h2 className="text-lg text-foreground">{mainCTAs[4].title}</h2>
+          <Link href={mainCTAs[4].href}>
+            <Button variant="ghost" className="flex items-center gap-1">
+              Explore all <ChevronRight className="h-4 w-4" />
+            </Button>
+          </Link>
+        </div>
+        <p className="text-sm text-muted-foreground md:w-3/4"></p>
+      </div>
+      <div className="grid grid-cols-1 gap-6">
+        {books.slice(0, 9).map((item: any, index: any) => {
+          return <BookItem key={index} item={item} />;
+        })}
+      </div>
+    </div>
+  );
+}

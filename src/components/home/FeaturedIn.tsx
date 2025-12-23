900f5d7182082c236d675baebb7e629706bf4e0f
jaygal <joshuagalinato@gmail.com>
2025-12-23 08:13:39 +0100

    updated featured in

--- src/components/home/FeaturedIn.tsx
+++ src/components/home/FeaturedIn.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export function FeaturedIn() {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-4">
       <h3 className="text-center text-xl text-muted-foreground">Featured in</h3>
       <div className="mx-auto flex gap-12">
         <a
@@ -15,25 +15,10 @@ export function FeaturedIn() {
           <Image
             src="/logos/toools.design.png"
             alt="Tools.Design Logo"
-            width={100}
-            height={50}
+            width={200}
+            height={100}
             quality={70}
-            className="rounded-sm object-cover grayscale transition-all duration-500 ease-in-out hover:rounded-none hover:grayscale-0"
-            unoptimized
-          />
-        </a>
-        <a
-          href="https://tldr.tech/design/2025-10-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/logos/tldr.png"
-            alt="TLDR Logo"
-            width={100}
-            height={50}
-            quality={70}
-            className="rounded-sm object-cover grayscale transition-all duration-500 ease-in-out hover:rounded-none hover:grayscale-0"
+            className="w-40 rounded-sm object-cover grayscale transition-all duration-500 ease-in-out hover:rounded-none hover:grayscale-0"
             unoptimized
           />
         </a>
@@ -45,10 +30,25 @@ export function FeaturedIn() {
           <Image
             src="/logos/ux-design-weekly.png"
             alt="uxdesignweekly Logo"
-            width={50}
-            height={50}
+            width={100}
+            height={100}
             quality={70}
-            className="rounded-sm object-cover grayscale transition-all duration-500 hover:rounded-none hover:grayscale-0"
+            className="w-20 rounded-sm object-cover grayscale transition-all duration-500 hover:rounded-none hover:grayscale-0"
+            unoptimized
+          />
+        </a>
+        <a
+          href="https://tldr.tech/design/2025-10-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/logos/tldr.png"
+            alt="TLDR Logo"
+            width={200}
+            height={100}
+            quality={70}
+            className="w-40 rounded-sm object-cover grayscale transition-all duration-500 ease-in-out hover:rounded-none hover:grayscale-0"
             unoptimized
           />
         </a>

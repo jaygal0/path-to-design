577d76f07b69e844b4bf17a99cdf4f2b332a4b74
jaygal <joshuagalinato@gmail.com>
2026-03-19 09:02:45 +0100

    visible in apps

--- src/app/(pages)/best-design-apps/[slug]/page.tsx
+++ src/app/(pages)/best-design-apps/[slug]/page.tsx
@@ -169,9 +169,8 @@ export default async function AppDetailPage(props: {
           </div>
         </div>
       </div>
-      <div className="col-span-1">
-        {/* <div className="sticky top-20 flex flex-col gap-8"> */}
-        <div className="sticky top-20 hidden flex-col gap-8">
+      <div className="col-span-1 hidden md:block">
+        <div className="sticky top-20 flex flex-col gap-8">
           <NewsletterSidebar designers={designers.length} />
         </div>
       </div>

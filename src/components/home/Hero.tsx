1920686212d1470bdecc84785cbd8d0ceb704043
jaygal <joshuagalinato@gmail.com>
2026-04-03 09:11:44 +0200

    capitalise headings

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -12,7 +12,7 @@ export function Hero({ designers }: Props) {
   return (
     <div className="flex flex-col items-center">
       <div className="w-full space-y-3 text-center md:w-3/5">
-        <h1 className="text-4xl font-bold md:text-7xl">
+        <h1 className="text-4xl font-bold capitalize md:mx-auto md:text-7xl">
           {companyInfo.copy.heading}
         </h1>
         <h2 className="text-xl font-extralight text-muted-foreground md:text-3xl">
--- src/components/share-your-path/Hero.tsx
+++ src/components/share-your-path/Hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
   return (
     <div className="flex flex-col items-center">
       <div className="space-y-3 text-center md:w-4/5">
-        <h1 className="text-4xl font-bold md:text-7xl">
+        <h1 className="text-4xl font-bold capitalize md:text-7xl">
           Shape how the next generation enters design
         </h1>
         <h2 className="mx-auto pb-8 text-xl font-extralight text-muted-foreground md:w-3/4 md:text-2xl">
@@ -15,9 +15,7 @@ export function Hero() {
           designers to help people break into the industry.
         </h2>
         <Link href="share-your-path/form">
-          <Button className="w-full md:w-min">
-            Share your path
-          </Button>
+          <Button className="w-full md:w-min">Share your path</Button>
         </Link>
       </div>
     </div>
